52cd4cbe52e8b00012a35ca7d05cdb7f148f4ab5
Pranav <naringrekarpranav6@live.com>
2022-05-06 21:12:47 +0530

    Redux Toolkit C1

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,49 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-import { INCREMENT, DECREMENT, INCREASE, TOGGLE } from '../utils/constants';
+// Initial states
+const initialCounterState = { counter: 0, showCounter: true };
+const initialAuthState = { isAuthenticated: false };
 
-const initialState = { counter: 0, showCounter: true };
+// store slices
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return { ...state, counter: state.counter + 1 };
-    case INCREASE:
-      return { ...state, counter: state.counter + action.amount };
-    case DECREMENT:
-      return { ...state, counter: state.counter - 1 };
-    case TOGGLE:
-      return { ...state, showCounter: !state.showCounter };
-    default:
-      return state;
-  }
-};
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+// Combining slices(reducers)
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+//exporting actions
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
-
-// const counterSubscriber = () => {
-//   const latestState = store.getState();
-//   console.log(latestState);
-// };
-
-// useSelector will automatically do this for us
-// store.subscribe(counterSubscriber);
-
-// store.dispatch({ type: 'INCREMENT', payload: 69 });
